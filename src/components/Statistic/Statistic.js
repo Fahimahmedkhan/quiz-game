99416f6c6be356c3088f0caa905a7065eca4217f
fahimahmedkhan <rolx9999@gmail.com>
2022-10-11 16:00:55 +0600

    statistic section

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip
+} from "recharts";
 
 const Statistic = () => {
+    const loaderData = useLoaderData();
+    const statistic = loaderData.data;
+
     return (
-        <div>
-            <h1>this is statistic</h1>
+        <div className='flex justify-center items-center m-10'>
+            <LineChart
+                width={800}
+                height={500}
+                data={statistic}
+            >
+
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Line
+                    connectNulls
+                    type="monotone"
+                    dataKey="total"
+                    stroke="#8884d8"
+                    fill="#8884d8"
+                />
+            </LineChart>
         </div>
     );
 };
